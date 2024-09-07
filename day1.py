12aaed6ed50116a89c8b651d359d291f9161c6aa
irsyadabdillah18 <muhammadirsyadabdillah18@gmail.com>
2024-09-07 11:02:00 +0700

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,37 @@
+#a = "apel" # "a" adalah variabel dan "apel" adalah data
+# print(a)
+
+# buah = "jeruk" #ini adalah tipedata 'string'
+# harga = 15000 #ini adalah tipedata 'integer'
+# berat = 1,5 #ini adalah tipedata 'float'
+# beli = true #ini adalah tipedata 'boolean'
+
+#print(buah)
+#print(harga)
+#print(berat)
+#print(beli)
+
+# format string
+nama = 'siti'
+umur = 16
+alamat = 'jl.buntu'
+tinggi = 1.75
+
+print (f"nama saya {nama}.saya berusia {umur}tahun,dan tinggi saya {tinggi} meter. saya tinggal di {alamat}.")
+
+#input
+input("siapa nama kamu?")
+print(f"halo, {nama}")
+
+umur = int(input("umur kamu berapa?"))
+print(f"{umur}tahun")
+
+print("halo, perkenalkan nama aku bot !")
+print("halo, bot !")
+nama = input("nama, kamu siapa?")
+print(f"namaku {nama}")
+print(f"namaku {nama}, salam kenal!")
+print(f"kamu sibuk apa sekarang, {nama}?")
+kegiatan = input("kesibukanmu?")
+print(f"aku lagi sibuk {kegiatan} sekarang.")
+print(f"oke,semoga{kegiatan} berhasil.")
